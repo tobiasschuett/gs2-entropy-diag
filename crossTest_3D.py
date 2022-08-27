8c47f109d66c7e644e56a1bf0e9c41b8bb1945f4
TM Schuett <tms535@login2.pri.viking.alces.network>
2022-08-27 15:00:24 +0100

    3D now works as well as crossTest_3D shows, crossTest_4D also still works as before

--- crossTest_3D.py
+++ crossTest_3D.py
@@ -4,14 +4,28 @@ import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 data = nc.Dataset("nlinear1/test4/c1.out.nc") #test2 folder is baseline that works
-S_transfer = np.array(data["entropy_transfer"][:])
+S_transfer = np.array(data["entropy_transfer_3D"][:])
 
 S_transfer_python = np.load("Stransfer-test2_entropy.npz")
+#sum along theta,energy,lamda,sign; left over kys,kyt,kxs,kxt
 S_transfer_python_sum = np.sum(S_transfer_python["entropy_result"],axis=(0,1,2,3))
+
+#make 3D
+kx = S_transfer_python["kx"]
+nkx = len(kx)
+ky = S_transfer_python["ky"]
+nky = len(ky)
+
+ikx0 = np.argmin(np.abs(kx))
+iky0 = np.argmin(np.abs(ky))
+ikyt = iky0
+
+S_transfer_python_sum = S_transfer_python_sum[:,ikyt,:,:]
 
 digit_precision = 8
 
-A = np.round(S_transfer[-1,:,:,:,:],digit_precision)
+# set last timestep with "-1" and set kyt = 0 with "0"
+A = np.round(S_transfer[-1,:,:,:],digit_precision)
 B = np.round(S_transfer_python_sum,digit_precision)
 
 resultsAgree = (A==B).all()
